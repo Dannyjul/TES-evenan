c5f3add2e9aa5637698827f5a7fb8dbf1d6ffdcf
Evania Joycelin <evaniajoycelin@gmail.com>
2021-04-14 09:18:29 +0800

    Bug No 2

--- vendor/tambahan/tools.js
+++ vendor/tambahan/tools.js
@@ -33,68 +33,68 @@ function uploadFoto(input) {
 
 
 	$(document).ready(function() {
-	  //var cek = $('#tampilPassword').val();
-	  $('.tampilPassword').click(function() {
-	    if ($(this).is(':checked')) {
-	      $('.tampilinPass').attr('type', 'text');
-	    } else {
-	      $('.tampilinPass').attr('type', 'password');
-	    }
-    });
+	  // //var cek = $('#tampilPassword').val();
+	  // $('.tampilPassword').click(function() {
+	  //   if ($(this).is(':checked')) {
+	  //     $('.tampilinPass').attr('type', 'text');
+	  //   } else {
+	  //     $('.tampilinPass').attr('type', 'password');
+	  //   }
+    // });
 
-    $(".passTaksama").hide();
+    // $(".passTaksama").hide();
     
-    $('#pass2').keyup(function (e) 
-    { 
-      var pass1 = $(".tampilinPass").val();
-      var pass2 = $("#pass2").val();
+    // $('#pass2').keyup(function (e) 
+    // { 
+    //   var pass1 = $(".tampilinPass").val();
+    //   var pass2 = $("#pass2").val();
 
-      if (pass1=="" && pass2=="") 
-      {
-        $("#pass2").removeClass("merahAlert"); 
-        $("#tampilinPass").removeClass("merahAlert"); 
-        $(".passTaksama").hide();
-      } 
-      else 
-      {
-        if (pass2!=pass1) 
-        {
-          $("#pass2").addClass("merahAlert"); 
-          $(".tampilinPass").addClass("merahAlert"); 
-          $(".passTaksama").fadeIn(1000);
-          //return false; 
-        } 
-        else 
-        {
-          $("#pass2").removeClass("merahAlert"); 
-          $(".tampilinPass").removeClass("merahAlert"); 
-          $("#pass2").addClass("hijauAlert"); 
-          $(".tampilinPass").addClass("hijauAlert"); 
-          $(".passTaksama").fadeOut(1000);
-          //return true; 
-        }
-      }
+    //   if (pass1=="" && pass2=="") 
+    //   {
+    //     $("#pass2").removeClass("merahAlert"); 
+    //     $("#tampilinPass").removeClass("merahAlert"); 
+    //     $(".passTaksama").hide();
+    //   } 
+    //   else 
+    //   {
+    //     if (pass2!=pass1) 
+    //     {
+    //       $("#pass2").addClass("merahAlert"); 
+    //       $(".tampilinPass").addClass("merahAlert"); 
+    //       $(".passTaksama").fadeIn(1000);
+    //       //return false; 
+    //     } 
+    //     else 
+    //     {
+    //       $("#pass2").removeClass("merahAlert"); 
+    //       $(".tampilinPass").removeClass("merahAlert"); 
+    //       $("#pass2").addClass("hijauAlert"); 
+    //       $(".tampilinPass").addClass("hijauAlert"); 
+    //       $(".passTaksama").fadeOut(1000);
+    //       //return true; 
+    //     }
+    //   }
 
-      $(".regist").click(function (e) 
-      { 
-        var pass1 = $(".tampilinPass").val();
-        var pass2 = $("#pass2").val();
-        if (pass2==pass1) 
-        {
-          return true;
-         // submi
+    //   $(".regist").click(function (e) 
+    //   { 
+    //     var pass1 = $(".tampilinPass").val();
+    //     var pass2 = $("#pass2").val();
+    //     if (pass2==pass1) 
+    //     {
+    //       return true;
+    //      // submi
           
          
-        } 
-        else 
-        {
-          return false;
-        }
+    //     } 
+    //     else 
+    //     {
+    //       return false;
+    //     }
         
-      });
+    //   });
 
       
-    });
+    // });
  
 
     $('.slick').slick({
@@ -141,6 +141,162 @@ function uploadFoto(input) {
 
   
 	});
+
+//FORM VALIDATION
+//CREATED BY EVA 13-04-2021
+$(document).ready(function(){
+  //CREATED BY DANNY LONG TIME AGO
+  //var cek = $('#tampilPassword').val();
+      $('.tampilPassword').click(function() {
+        if ($(this).is(':checked')) {
+          $('.tampilinPass').attr('type', 'text');
+        } else {
+          $('.tampilinPass').attr('type', 'password');
+        }
+      });
+  
+      $(".passTaksama").hide();
+  
+  //CREATED BY EVA 13-04-2021
+
+  function validateName(){
+    var nama = $("#nama").val();
+    var stringPattern = /^[a-zA-Z ]*$/;
+    
+    if(nama == ""){
+      $('#errNama').css("display", "block");
+      $('#errNama').html("*Please input your name.");
+    }else if (stringPattern.test(nama) == false){
+      $('#errNama').css("display", "block");
+      $('#errNama').html("*Your name contains number.");
+    }else{
+      $('#errNama').css("display", "none");
+    }
+  }
+
+  function validateAlamat(){
+    var alamat = $("#alamat").val();
+
+    if(alamat == ""){
+      $('#errAlamat').css("display", "block");
+      $('#errAlamat').html("*Please input your address.");
+    }else{
+      $('#errAlamat').css("display", "none");
+    }
+  }
+  
+  function validatePhone(){
+    var phone = $("#no-hp").val();
+    var phonePattern = /^08[0-9]*$/;
+
+    if(phone == ""){
+      $('#errPhone').css("display", "block");
+      $('#errPhone').html("*Please input your phone number.");
+    }else if(phonePattern.test(phone) == false || phone.length > 13){
+      $('#errPhone').css("display", "block");
+      $('#errPhone').html("*Wrong phone number.");
+    }else{
+      $('#errPhone').css("display", "none");
+    }
+  }
+
+  function validateEmail(){
+    var email = $("#email").val();
+    var emailPattern = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if(email == ""){
+      $('#errEmail').css("display", "block");
+      $('#errEmail').html("*Please input your email.");
+    }else if(emailPattern.test(email) == false){
+      $('#errEmail').css("display", "block");
+      $('#errEmail').html("*Wrong email.");
+    }else{
+      $('#errEmail').css("display", "none");
+    }
+  }
+
+  function validatePass(){
+    var pass = $("#pass").val();
+
+    if(pass == ""){
+      $('#errPass').css("display", "block");
+      $('#errPass').html("*Please input your Password.");
+    }else{
+      $('#errPassConf').css("display", "none");
+    }
+  }
+
+  function validateCPass(){
+    var confirm_pass = $("#confirm-pass").val();
+    var pass = $("#pass").val();
+
+    if(confirm_pass == ""){
+      $('#errPassConf').css("display", "block");
+      $('#errPassConf').html("*Please reinput the password.");
+    }else if(pass != confirm_pass){
+      $('#errPassCon').css("display", "block");
+      $('#errPassConf').html("*Password didn't match.");
+    }else{
+      $('#errPassConf').css("display", "none");
+    }
+  }
+
+  function validateBirth(){
+    var birth = $('#tgl-lahir input[name="tglahir"]').val();
+    if(birth == "dd/mm/yyyy"){
+      $('#errTgl').css("display", "block");
+      $('#errTgl').html("*Please select your birthdate.");
+    }else{
+      $('#errTgl').css("display", "none");
+    }
+  }
+
+  function validateGender(){
+    var gender = $('#gender').val();
+    if(gender == ""){
+      $('#errGender').css("display", "block");
+      $('#errGender').html("*Please select your gender.");
+    }else{
+      $('#errGender').css("display", "none");
+    }
+  }
+
+  $('#nama').on('keyup', function (e) { 
+    validateName();
+  });
+
+  $('#alamat').on('keyup', function (e) { 
+    validateAlamat();
+  });
+
+  $('#no-hp').on('keyup', function (e) { 
+    validatePhone();
+  });
+
+  $('#email').on('keyup', function (e) { 
+    validateEmail();
+  });
+
+  $('#pass').on('keyup', function (e) { 
+    validatePass();
+  });
+
+  $('#confirm-pass').on('keyup', function (e) { 
+    validateCPass();
+  });
+
+  $('#btn-modal-daftar').on('click', function(e){
+    validateEmail();
+    validateAlamat();
+    validateBirth();
+    validateEmail();
+    validatePhone();
+    validateEmail();
+    validatePass();
+    validateCPass();
+  });
+
+});
 
   
     
