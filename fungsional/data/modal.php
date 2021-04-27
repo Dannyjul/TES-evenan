00f39e07f2a4dd5447ec1d52bb92e6eebe1142bd
Vincent <vincent@aleph-labs.com>
2021-04-27 18:00:02 +0700

    fix modal terms and condition

--- fungsional/data/modal.php
+++ fungsional/data/modal.php
@@ -1,5 +1,5 @@
 <style>
-  #pesanAwal, #pesanUpgrade
+  #pesanAwal, #pesanUpgrade, #setujuan
   {
     display: none;
     width: 700px;
@@ -7,15 +7,24 @@
     color: white;
   }
 
-  #pesanAwal img, #pesanUpgrade img
+  #pesanAwal img, #pesanUpgrade img, #setujuan img
   {
     display: block;
     margin: auto;
   }
 
-  #pesanAwal a, #pesanUpgrade a
+  #pesanAwal a, #pesanUpgrade a, #setujuan a
   {
     width: 100%;
+  }
+  #setujuan
+  {
+    width: 500px;
+  }
+
+  #setujuan p
+  {
+    margin-top: 25px;
   }
 </style>
 
@@ -158,7 +167,7 @@
      
           <div class="container-input-field">
             <input type="checkbox" name="setujuan" id="tc">
-            <label class="tc" for="tc">Saya telah membaca dan menyetujui<span><a data-toggle="modal" data-target="#Setujuan" href="#">Syarat dan Ketentuan</a></span></label>
+            <label class="tc" for="tc">Saya telah membaca dan menyetujui<span><a data-fancybox data-src='#setujuan' href='javascript:;'>Syarat dan Ketentuan</a></span></label>
             <p class = "error-msg" id="errCheckBox">Error Message</p>
           </div>
 
@@ -208,5 +217,67 @@
         </div>
     </div>
 </div>
-
 <script src="./vendor/tambahan/modal.js"></script>
+
+
+<?php
+
+    $perintah = $crud->eksekusiSQl("SELECT *FROM info 
+                                    INNER JOIN user
+                                        ON user.id_user= info.id_user
+                                    WHERE info.jenis_info='Term and Condition'
+                                    ");
+    $hitung   = $crud->hitungData($perintah);
+
+
+    foreach($perintah as $a)
+    {
+        $idinfo= $a['id_info'];
+        $jenis = $a['jenis_info'];
+        $foto  = $a['foto_info'];
+        $tgl   = $a['tgl_info'];
+        $desk  = $a['deskripsi'];
+
+
+        $nus    = $a['nama_user'];
+
+        if ($foto=='Kosong') 
+        {
+            //$gambar = "<img class='rounded-circle' src='./img/nofoto.png' width='50' height='50'>";
+            $box = "";
+        } 
+        else 
+        {
+            $tujuan = "./foto/info/$foto";
+           /* $gambar = 
+            "   <a data-fancybox='gallery' href='$tujuan' data-caption='$jenis'>
+                    <img src='$tujuan' width='50' height='50'>
+                </a>
+            ";*/
+            $box = 
+            "
+                <center>
+                <img src='$tujuan' width='70%' height='200'>
+                </center>
+            ";
+        }
+    }
+
+
+
+
+?>
+<div id="setujuan">
+
+  <center>
+    <h4><?php echo $jenis; ?></h4>
+  </center>
+
+  <?php
+      echo
+      "
+          $box
+          $desk
+      ";
+  ?>
+</div>
